4a5ce62469c091d3a4dd7b2de426f762c3401f03
Sebastian Lis <pocztasebastian1306@gmail.com>
2021-05-26 19:48:25 +0200

    ramove unnecessary code

--- src/pl/nauka/Game.java
+++ src/pl/nauka/Game.java
@@ -1,15 +1,9 @@
-/*
- *Założenia:
- * 1. Hasło do odgadnięcia od 1 do 10 znaków.
- * 2. Bez używania polskich znaków.
- */
+
 
 package pl.nauka;
 
-import pl.nauka.graphic.HangmanGraphic;
 import pl.nauka.hangman.Hangman;
 
-import java.util.Scanner;
 
 public class Game {
 
